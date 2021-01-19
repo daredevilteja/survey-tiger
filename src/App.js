7167e4f6e6fb97f283079981a08beb45d5fe2a3f
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-01-19 22:49:38 +0530

    Updated the styling of App

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
         <Switch>
           <Route path="/create">Create Survey</Route>
           <Route path="/take">Take Survey</Route>
           <Route path="/">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <Link to="/create">
-                <Button className="survey-main-btn">Create Survey</Button>
-              </Link>
-              <Link to="/take">
-                <Button className="survey-main-btn">Take Survey</Button>
-              </Link>
-            </header>
+            <Link to="/create">
+              <Button className="survey-main-btn">Create Survey</Button>
+            </Link>
+            <Link to="/take">
+              <Button className="survey-main-btn">Take Survey</Button>
+            </Link>
           </Route>
         </Switch>
       </div>
