99f994a3ca7842ac447e646c54e7ba0f92eaa9e1
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-01-19 22:39:39 +0530

    Created routes for buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from "./logo.png";
 import "./App.css";
 import { Button } from "reactstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button className="survey-main-btn">Create Survey</Button>
-        <Button className="survey-main-btn">Take Survey</Button>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/create">Create Survey</Route>
+          <Route path="/take">Take Survey</Route>
+          <Route path="/">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Link to="/create">
+                <Button className="survey-main-btn">Create Survey</Button>
+              </Link>
+              <Link to="/take">
+                <Button className="survey-main-btn">Take Survey</Button>
+              </Link>
+            </header>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
